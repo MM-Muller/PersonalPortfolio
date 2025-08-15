8ca1d4fd140962c11594a501cf5e7e0b62487227
Matheus Müller <142352374+MM-Muller@users.noreply.github.com>
2025-08-15 15:08:24 -0300

    style(navbar): correção no alinhamento e espaçamento

--- JS/navigation.js
+++ JS/navigation.js
@@ -10,21 +10,4 @@ function scrollToSection(sectionId) {
 
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.button-container .button');
-
-    buttons.forEach(button => {
-        button.addEventListener('mouseenter', function () {
-            this.style.transform = 'translateY(-3px) scale(1.1)';
-        });
-
-        button.addEventListener('mouseleave', function () {
-            this.style.transform = 'translateY(0) scale(1)';
-        });
-
-        button.addEventListener('click', function () {
-            this.style.transform = 'translateY(0) scale(0.95)';
-            setTimeout(() => {
-                this.style.transform = 'translateY(-3px) scale(1.1)';
-            }, 150);
-        });
-    });
 });
